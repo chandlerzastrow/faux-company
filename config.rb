9b3ceeef0f7a1dc7f9549569758c2640d016adf8
Chandler Zastrow <chandlerzastrow@rgnt2-105-104-dhcp.int.colorado.edu>
2016-10-25 20:35:15 -0600

    created grids scss

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 require 'compass/import-once/activate'
+require 'susy'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
@@ -13,7 +14,7 @@ javascripts_dir = "js"
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
